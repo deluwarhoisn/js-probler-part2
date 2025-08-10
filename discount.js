1896fe1bb74da99f83f51ca19629926881ed1b9a
deluwarhoisn <babuhossen301@gmail.com>
2025-08-11 00:46:17 +0600

    advanced

--- discount.js
+++ discount.js
@@ -20,3 +20,32 @@ function discountedPrice(quantity){
 
 const total = discountedPrice(5);
 console.log(total);
+
+//-------------
+
+function layeredDiscountedTotal (quantity){
+    const first100price = 100;
+    const second100price = 90;
+    const above200price = 70;
+    if(quantity <= 100){
+        const total = quantity * first100price;
+        return total;
+    }
+    else if(quantity <= 200){
+        const first100price = 10 * first100price;
+        const remainingQuantity = quantity - 100;
+        const remainingTotal = remainingQuantity * second100price;
+        const total = first100Total + remainingTotal;
+        return total;
+    }
+    else{
+        const first100price = 100 * first100price;
+        const second100total = 100 * second100price;
+        const remainingQuantity = quantity - 200;
+        const remainingTotal = remainingQuantity * above200price;
+        const total = first100price + second100price + remainingTotal;
+        return total;
+    }
+}
+const all =layeredDiscountedTotal(34)
+console.log(all)
