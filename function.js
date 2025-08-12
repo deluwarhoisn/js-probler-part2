2534587ebe54bc969307a28e0b44abcc55d42138
deluwarhoisn <babuhossen301@gmail.com>
2025-08-12 23:39:17 +0600

    valid-contact

--- function.js
+++ function.js
@@ -91,3 +91,32 @@ function validProposal(person1, person2) {
   } else return true;
 }
 console.log(validProposal(1, { name: "bjoy", gender: "male", age: 32 }));
+
+
+
+function validContact(contact) {
+  if (typeof contact !== "string") {
+    return "Invalid";
+  }
+  if (
+    contact.length == 11 &&
+    contact.startsWith("01") == true &&
+    contact.includes(" ") != true
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(validContact("01819834567"));
+// negative
+// if (contact.length != 11) {
+//     return false;
+//   }
+//   if (contact.startsWith("01") == false) {
+//     return false;
+//   }
+//   if (contact.includes(" ") == true) {
+//     return false;
+//   }
+//   return true;
