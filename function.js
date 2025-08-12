70b3d73c443959c0314b8617f8f6749be1568e64
deluwarhoisn <babuhossen301@gmail.com>
2025-08-12 22:02:03 +0600

    erore code

--- function.js
+++ function.js
@@ -1,7 +1,28 @@
-function square(input){
-    let squareNew = input * input;
-    console.log(squareNew);
-    return squareNew;
+// function square(input){
+//     let squareNew = input * input;
+//     console.log(squareNew);
+//     return squareNew;
+// }
+// let result = square(11);
+// console.log(result);
+
+
+//---------------- function
+
+function sun(input1,input2){
+    let result = input1 + input2;
+    // console.log(result);
+    return result;
 }
-let result = square(10);
+
+function multiply(input1, input2){
+    let result = input1 * input2;
+    // console.log(result);
+    return result;
+}
+
+let a = 2;
+let b = 4;
+
+const result = sum (a,b) + multiply (a,b);
 console.log(result);
