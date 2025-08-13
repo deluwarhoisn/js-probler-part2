b0b06349e0d4330c74aaf2f7c1926d194f5a6c48
deluwarhoisn <babuhossen301@gmail.com>
2025-08-13 22:59:43 +0600

    problem-2.js

--- asinment-4/problem-1.js
+++ asinment-4/problem-1.js
@@ -17,3 +17,28 @@ console.log(totalFine(552));
 console.log(totalFine(-35));  
 console.log(totalFine("65")); 
 console.log(totalFine("Gurubd tai ticket korenai")); 
+
+//-------------> problem-2
+
+function onlyCharacter(str) {
+  if (typeof str !== 'string') {
+    return "Invalid";
+  }
+
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9') || char === '-' || char === '@' || char === ':') {
+      result += char;
+    }
+  }
+  return result.toUpperCase();
+}
+
+console.log(onlyCharacter("h e l l o wo r l d")); 
+console.log(onlyCharacter("Cy bar-At tac k")); 
+console.log(onlyCharacter("hac k m e 1 @ru.c n"));
+console.log(onlyCharacter("Ser ver :: Do wn")); 
+
+console.log(onlyCharacter(["hack", "me"])); 
+console.log(onlyCharacter(true)); 
